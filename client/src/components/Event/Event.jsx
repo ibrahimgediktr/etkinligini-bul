312722689438d4d8f83e92e686a4afe372203eda
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-06-22 22:47:52 +0300

    update Event components

--- client/src/components/Event/Event.jsx
+++ client/src/components/Event/Event.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+import 'moment/locale/tr'
 function Event({ event }) {
+  moment.locale("tr");
+
   return (
     <li key={event?.id} className="event__item">
-      <Link to={`events/${event?.id}`}>
+      <Link to={`/events/${event?.id}`}>
         <div className="image__container">
           <img src={`/${event?.images[0]}`} alt="" className="image" />
         </div>
         <div className="texts">
-          <h6>{event?.name}</h6>
-          <p>{event?.city}</p>
-          <p>{event?.address}</p>
+          <h6 className="name">{event?.name}</h6>
+          <p className="city">{event?.city}</p>
+          <p className="address">{event?.address}</p>
+          <span className="date">{moment(event?.start_date).fromNow()}</span>
         </div>
       </Link>
     </li>
