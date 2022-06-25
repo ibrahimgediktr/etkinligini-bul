38551923c02d5617d73bde598f8ad513a103905b
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-06-25 11:38:15 +0300

    update Filter component
    fix date picker end date issue

--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -5,12 +5,11 @@ import tr from "date-fns/locale/tr";
 import "react-datepicker/dist/react-datepicker.css";
 import { useNavigate } from "react-router-dom";
 
-
 function Filter({ events, categories, filteredEvents, setFilteredEvents }) {
   const navigate = useNavigate();
   const [selectedDate, setSelectedDate] = useState({
-    start_date: new Date(),
-    end_date: new Date(),
+    start_date: null,
+    end_date: null,
   });
   const [filteredKeys, setFilteredKeys] = useState({
     searchTerm: "",
@@ -20,38 +19,51 @@ function Filter({ events, categories, filteredEvents, setFilteredEvents }) {
     city: "",
   });
 
-  const filterStartDate = moment(selectedDate?.start_date).format("YYYY/MM/DD");
-  const filterEndDate = moment(selectedDate?.end_date).format("YYYY/MM/DD");
-
-  useEffect(() => {
+  const handleDateChange = (dates) => {
+    const [start, end] = dates;
+    setSelectedDate((prevObject) => ({
+      ...prevObject,
+      start_date: start,
+      end_date: end,
+    }));
     setFilteredKeys((prevObject) => ({
       ...prevObject,
-      start_date: filterStartDate,
-      end_date: filterEndDate,
+      start_date: moment(start).format("YYYY/MM/DD"),
+      end_date: moment(end).format("YYYY/MM/DD"),
     }));
-    
-  }, [filterStartDate, filterEndDate]);
+  };
 
   const filtered = () => {
     const updatedEvents = events?.filter((event) => {
-      var filterName = filteredKeys.searchTerm.length !== 0 ? event.name
-        .toLocaleLowerCase()
-        .includes(filteredKeys.searchTerm.toLocaleLowerCase()) : event
-      var filterCategory = filteredKeys.category !== "0" ? event.category === parseInt(filteredKeys.category) : event
-      var filterCity = filteredKeys.city.length !== 0 ? event.city
-        .toLocaleLowerCase()
-        .includes(filteredKeys.city.toLocaleLowerCase()) : event
-      var filterDate = filteredKeys?.start_date <= moment(event.start_date).format("YYYY/MM/DD") && moment(event.start_date).format("YYYY/MM/DD") <= filteredKeys.end_date
-      return filterName && filterCategory && filterCity && filterDate
+      var filterName =
+        filteredKeys.searchTerm.length !== 0
+          ? event.name
+              .toLocaleLowerCase()
+              .includes(filteredKeys.searchTerm.toLocaleLowerCase())
+          : event;
+      var filterCategory =
+        filteredKeys.category !== "0"
+          ? event.category === parseInt(filteredKeys.category)
+          : event;
+      var filterCity =
+        filteredKeys.city.length !== 0
+          ? event.city
+              .toLocaleLowerCase()
+              .includes(filteredKeys.city.toLocaleLowerCase())
+          : event;
+      var filterDate =
+        filteredKeys?.start_date <=
+          moment(event.start_date).format("YYYY/MM/DD") &&
+        moment(event.start_date).format("YYYY/MM/DD") <= filteredKeys.end_date;
+      return filterName && filterCategory && filterCity && filterDate;
     });
     setFilteredEvents(updatedEvents);
   };
 
-
   const handleFilter = (e) => {
     e.preventDefault();
     filtered();
-    navigate('/search');
+    navigate("/search");
   };
 
   return (
@@ -71,12 +83,11 @@ function Filter({ events, categories, filteredEvents, setFilteredEvents }) {
           <select
             name="category"
             className="filter__category input"
-           
             onChange={(e) =>
               setFilteredKeys({ ...filteredKeys, category: e.target.value })
             }
           >
-            <option value="0"  >Kategoriler</option>
+            <option value="0">Kategoriler</option>
             {categories?.map((category) => (
               <option key={category.id} value={category.id}>
                 {category.name}
@@ -95,24 +106,19 @@ function Filter({ events, categories, filteredEvents, setFilteredEvents }) {
           />
           <DatePicker
             selected={selectedDate.start_date}
-            onChange={(dates) => {
-              const [start, end] = dates;
-              setSelectedDate((prevObject) => ({
-                ...prevObject,
-                start_date: start,
-                end_date: end,
-              }));
-            }}
-            startDate={selectedDate.start_date}
-            endDate={selectedDate.end_date}
+            onChange={(dates) => handleDateChange(dates)}
+            startDate={selectedDate?.start_date}
+            endDate={selectedDate?.end_date}
             selectsRange
             className="input"
             placeholderText="Tarih"
             dateFormat="dd MMMM"
             locale={tr}
-            //   inline
+              // inline
           />
-          <button type="submit" className="btn btn__primary">Ara</button>
+          <button type="submit" className="btn btn__primary">
+            Ara
+          </button>
         </form>
       </div>
     </div>
