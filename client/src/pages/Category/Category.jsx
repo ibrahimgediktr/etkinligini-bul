2c68e41e8b8f6cc703e2063ba49346c2ee2ca0bd
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-06-25 14:42:15 +0300

    added loading

--- client/src/pages/Category/Category.jsx
+++ client/src/pages/Category/Category.jsx
@@ -3,18 +3,22 @@ import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchCategoryEvents } from "../../api/api";
 import Event from "../../components/Event/Event";
+import Loading from "../../components/Loading/Loading";
 
 function Category() {
   const { category_id } = useParams();
   const [categoryEvents, setCategoryEvents] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     (async () => {
       const data = await fetchCategoryEvents(category_id);
       setCategoryEvents(data);
+      setLoading(false);
     })();
   }, [category_id]);
+
+  if(loading) return <Loading />
 
   return (
     <div className="category__events">
