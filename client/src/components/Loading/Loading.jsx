2b98105bf0480c952e10399f942881bb8c7d9359
İbrahim Gedik <ibrahimgediktc@gmail.com>
2022-06-25 14:39:27 +0300

    created Loading components

---
+++ client/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="loading-container">
+      <img src="/loading.gif" alt="loading" />
+    </div>
+  );
+}
+
+export default Loading;
